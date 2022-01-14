72c9bd7ce2e0ae6a9a27ea65de94aec972df2139
Prajval Singh <prajvaltheking@gmail.com>
2022-01-14 20:41:59 +0530

    First state after organising

--- src/Components/Expenses/ExpenseItems.js
+++ src/Components/Expenses/ExpenseItems.js
@@ -1,15 +1,25 @@
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card"
+import { useState } from "react";
+import Card from "../UI/Card"
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+
+  const [title,setTitle] = useState(props.title)
+
+  const changeTitle = () => {
+    setTitle("updated")
+    console.log("We Updated the state")
+  }
+
   return (
       <Card className="expense-item">
         <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price"> {props.amount} </div>
       </div>
+      <button onClick={changeTitle}>Change Title</button>
     </Card>
   );
 }
