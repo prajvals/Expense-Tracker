c0ba0dc85fb13a25c9f6a14ff3391a302185d160
Prajval Singh <prajvaltheking@gmail.com>
2022-01-16 08:30:03 +0530

    Year Filters added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import Expenses from "./Components/Expenses/Expenses";
 import NewExpenses from "./Components/NewExpenses/NewExpenses";
+import { useState } from "react";
+
+const expenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenseRecord, setExpenseRecord] = useState(expenses)
 
   const onAddExpense = (newExpense) => {
     const expenseItem = {...newExpense,id:Math.random()}
     console.log(expenseItem)
-  }
-  const onFilterSelection = (filterSelection) => {
-    console.log(filterSelection)
-  }
+    setExpenseRecord((previousState)=>{
+      return [expenseItem, ...previousState];
+    });
+  };
+  
+
   return (
     <div>
       <NewExpenses onAddExpense={onAddExpense}/>
-      <Expenses expenseList={expenses} onFilterSelection = {onFilterSelection}/>
+      <Expenses expenseList={expenseRecord}/>
     </div>
   );
 }
--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -2,15 +2,23 @@ import './Expenses.css'
 import ExpenseItem from "./ExpenseItems";
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
+import { useState } from 'react';
 
 function Expenses(props) {
   const expenseList = props.expenseList;
+  const [filteredYear, setFilteredYear] = useState('2021')
+
+  const onFilterSelection = (filterSelection) => {
+    setFilteredYear(filterSelection)
+  }
+  const filteredList = expenseList.filter(expense => expense.date.getFullYear().toString() === filteredYear)
+
   return (
     <div>
     <Card className = "expenses">
-      <ExpenseFilter onFilterSelection = {props.onFilterSelection}/>
-      {expenseList.map(expense => 
-      <ExpenseItem title={expense.title} amount = {expense.amount} date ={expense.date}/>
+      <ExpenseFilter onFilterSelection = {onFilterSelection} yearSelected={filteredYear}/>
+      {filteredList.map(expense => 
+      <ExpenseItem key={expense.id} title={expense.title} amount = {expense.amount} date ={expense.date}/>
       )}
     </Card>
     </div>
